f6c3cd8c6e793b88f68afceca17c51d2c7574ff0
9um4 <51898564+9um4@users.noreply.github.com>
2024-07-22 23:05:54 +0900

    add: Check Vault API Dependency

--- src/main/kotlin/io/github/vitelotte/economy/Economy.kt
+++ src/main/kotlin/io/github/vitelotte/economy/Economy.kt
@@ -1,14 +1,34 @@
 package io.github.vitelotte.economy
 
+import org.bukkit.plugin.RegisteredServiceProvider
 import org.bukkit.plugin.java.JavaPlugin
 
 class Economy : JavaPlugin() {
 
+    companion object {
+        lateinit var economy: Economy
+    }
+
     override fun onEnable() {
-        // Plugin startup logic
+        if (!setupEconomy()) {
+            logger.severe("Vault plugin not found! Disabling plugin.")
+            server.pluginManager.disablePlugin(this)
+            return
+        }
+        logger.info("Plugin successfully enabled.")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        logger.info("Plugin successfully disabled.")
+    }
+
+    private fun setupEconomy(): Boolean {
+        if (server.pluginManager.getPlugin("Vault") == null) {
+            return false
+        }
+
+        val registerServiceProvider: RegisteredServiceProvider<Economy> = server.servicesManager.getRegistration(Economy::class.java) ?: return false
+        economy = registerServiceProvider.provider;
+        return true;
     }
 }
