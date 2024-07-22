54dbe9d0f06e1aa2048f3bd9ae0dcb1a73a0fb10
9um4 <51898564+9um4@users.noreply.github.com>
2024-07-22 22:59:27 +0900

    init: Init Project

---
+++ src/main/kotlin/io/github/vitelotte/economy/Economy.kt
@@ -0,0 +1,14 @@
+package io.github.vitelotte.economy
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Economy : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
