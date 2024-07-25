c19000656cb8c06ecf1402d82e68de9ba004f511
9um4 <51898564+9um4@users.noreply.github.com>
2024-07-26 02:30:31 +0900

    refactor: Using Extended Methods

--- src/main/kotlin/io/github/vitelotte/simpleeconomy/listeners/PlayerJoinListener.kt
+++ src/main/kotlin/io/github/vitelotte/simpleeconomy/listeners/PlayerJoinListener.kt
@@ -1,21 +1,20 @@
-package io.github.vitelotte.economy.listeners
+package io.github.vitelotte.simpleeconomy.listeners
 
-import io.github.vitelotte.economy.Economy
+import io.github.vitelotte.simpleeconomy.SimpleEconomy
+import io.github.vitelotte.simpleeconomy.extensions.initBalance
+import io.github.vitelotte.simpleeconomy.managers.ConfigManager
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.event.player.PlayerMoveEvent
 
 class PlayerJoinListener : Listener {
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player
-        val initialBalance = Economy.configManager.getInitialBalance()
 
-        if (!Economy.economy.hasAccount(player)) {
-            Economy.economy.createPlayerAccount(player)
-            Economy.economy.depositPlayer(player, initialBalance)
+        if (!SimpleEconomy.economy.hasAccount(player)) {
+            player.initBalance()
         }
     }
 
