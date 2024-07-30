e37dae887ce53e83e0d77d9c4a2d4e76ec19ecc0
9um4 <51898564+9um4@users.noreply.github.com>
2024-07-30 09:53:41 +0900

    feat: Add addBalance, subtractBalance Methods

--- src/main/kotlin/io/github/vitelotte/simpleeconomy/extensions/BalanceExtension.kt
+++ src/main/kotlin/io/github/vitelotte/simpleeconomy/extensions/BalanceExtension.kt
@@ -15,3 +15,13 @@ fun OfflinePlayer.initBalance() {
     SimpleEconomy.economy.createPlayerAccount(this)
     SimpleEconomy.economy.depositPlayer(this, SimpleEconomy.configManager.getInitialBalance())
 }
+
+fun OfflinePlayer.addBalance(balance: Double) {
+    SimpleEconomy.economy.depositPlayer(this, balance);
+}
+
+fun OfflinePlayer.subtractBalance(balance: Double) {
+    if (SimpleEconomy.economy.has(this, balance)) {
+        SimpleEconomy.economy.withdrawPlayer(this, balance)
+    }
+}
